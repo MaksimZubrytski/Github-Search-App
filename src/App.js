4e23b1889578a930034f6a7754481f6663076fd3
MaksimZubrytski <maksimxdd@gmail.com>
2021-05-18 13:38:46 +0300

    feat: add initial component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Initial from './components/Initial/Initial';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-        
+        <Initial />
       </main>
     </div>
   );
--- src/components/Initial/Initial.jsx
+++ src/components/Initial/Initial.jsx
@@ -3,9 +3,11 @@ import styles from './Initial.module.css';
 const Initial = () => {
   return (
     <div className={styles.wrapper}>
-
+      <span className={styles.ico}></span>
+      <span className={styles.text}>Start with searching
+        a GitHub user</span>
     </div>
   );
 }
 
-export default Header;
+export default Initial;
